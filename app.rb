466db802b392462ba3f298dbbcfa479ece699dd4
Clem Freeman and Joey Sandine <clemfree@gmail.com>
2015-01-27 10:42:35 -0800

    Create interface for listing and adding products

--- app.rb
+++ app.rb
@@ -4,3 +4,15 @@ also_reload("lib/**/*.rb")
 require("sinatra/activerecord")
 require("./lib/product")
 require("pg")
+
+get '/' do
+  @products = Product.all
+  erb :index
+end
+
+post '/add_product' do
+  name = params.fetch("name")
+  price = params.fetch("price").to_f
+  Product.create(name: name, price: price)
+  redirect '/'
+end
